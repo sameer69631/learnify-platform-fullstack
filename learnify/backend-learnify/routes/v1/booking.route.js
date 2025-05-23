91064306c23a99dcd6287f47cc18a892ce24e6f0
sameer69631 <msa69631@gmail.com>
2025-05-23 21:19:24 +0530

    update start script to use node instead of nodemon

--- learnify/backend-learnify/routes/v1/booking.route.js
+++ learnify/backend-learnify/routes/v1/booking.route.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const {protect, restrictTo} = require('../../middleware/auth')
-const bookingController = require('../../controllers/bookings.controller')
+const assyncHandler = require('../../helper/asyncHandler')
+const bookingController = require('../../controllers/bookings.controller');
+const asyncHandler = require('../../helper/asyncHandler');
 
 const router = express.Router();
 
-router.post("/add-booking", protect, bookingController.addBookings)
-router.get("/get-bookings-by-mentor/:mentorId", protect, bookingController.getBookingsByMentorId)
-router.get("/get-bookings-by-student/:studentId", protect, bookingController.getBookingsByStudentId)
+router.post("/add-booking", protect, assyncHandler(bookingController.addBookings))
+router.get("/get-bookings-by-mentor/:mentorId", protect, asyncHandler(bookingController.getBookingsByMentorId))
+router.get("/get-bookings-by-student/:studentId", protect, asyncHandler(bookingController.getBookingsByStudentId))
 
 module.exports = router;
--- learnify/backend-learnify/utils/bookingStatus.js
+++
@@ -1,4 +0,0 @@
-
-const giveBookingStatus = (date, time) => {
-
-}
